945a8c9b140aa4080641b96298879eec22b4a12e
siva831997 <sivanandhu.ece97@gmail.com>
2017-06-19 22:37:03 +0530

    Update checker.java

--- checker.java
+++ checker.java
@@ -1,9 +1,7 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+
 import java.util.HashSet;
 import java.util.Set;
-
-public class Pangram {
+ class checker{
 
 public static void main(String args[]) {
 
