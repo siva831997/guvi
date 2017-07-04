f9e0ca523e0602d395d03cba7034ab58aace7347
siva831997 <sivanandhu.ece97@gmail.com>
2017-07-04 11:36:56 +0530

    Create Stringcamel.java

---
+++ Stringcamel.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Count {
+public static void main(String[] args) {
+	 Scanner s  = new Scanner(System.in);
+     System.out.print("Enter a sentence: ");
+     String input = s.nextLine();
+     String r = "";
+     char firstChar = input.charAt(0);
+     r = r + Character.toUpperCase(firstChar);
+     for (int i = 1; i < input.length(); i++) {
+         char currentChar = input.charAt(i);
+                 char previousChar = input.charAt(i - 1);
+                if (previousChar == ' ') {
+             r = r + Character.toUpperCase(currentChar);
+         } else {
+             r = r + currentChar;
+         }
+     }
+     System.out.println(r);
+ }
+
+
+}
